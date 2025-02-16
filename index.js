fed42aa28f834b465720f0871578d552c4664801
Nevskiy911 <122637230+Nevskiy911@users.noreply.github.com>
2025-02-16 18:20:00 +0100

    Initialize gh-pages

---
+++ index.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DTzD-3L3.js";document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector('a[href="/1-timer.html"]'),t=document.querySelector('a[href="/2-snackbar.html"]');e.addEventListener("click",n=>{console.log("Redirecting to Gallery")}),t.addEventListener("click",n=>{console.log("Redirecting to User Form")})});
+//# sourceMappingURL=index.js.map
