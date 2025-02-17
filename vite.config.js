5b21a5fbc90d92ff05807054b752600696bb7700
Nevskiy911 <maleev1820@gmail.com>
2025-02-17 19:28:26 +0100

    done hw

--- vite.config.js
+++ vite.config.js
@@ -36,7 +36,7 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
       emptyOutDir: true,
-      base: 'goit-js-hw-10',
+      base: 'goit-js-hw-11',
     },
     plugins: [
       injectHTML(),
